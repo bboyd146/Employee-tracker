0ba23c12895f8953e3165cdd2f3ab91605722b3f
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-13 16:17:57 -0500

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -13,5 +13,19 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id)
+)
