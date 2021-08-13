1719467e998f022ea71c7ba9520a75f515491292
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-13 18:37:49 -0500

    added seeds

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+
+app.listen(PORT, () => console.log(`lsitening on port ${PORT}`));
