d6ca028b41605d96ed634982f18cb680831a3511
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-13 15:52:38 -0500

    created 2 tables in db

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,16 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    ON DELETE SET NULL
+);
