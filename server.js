f0b88abe4394e19c73c84326f4ad7b89a0c72d2e
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-09-03 17:47:40 -0500

    View All Employees/Department/Roles, Quit, is functional

--- server.js
+++ server.js
@@ -1,114 +1,114 @@
-const getConnection = require('./config/connection');
-const express = require('express');
-const app = express();
+// const getConnection = require('./config/connection');
+// const express = require('express');
+// const app = express();
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('console.table');
+// const connection = getConnection();
 
-const { getQuery } = require('./orm');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-app.get('/api/company', (req, res) => {
-    getQuery('SELECT * FROM movies;', req, res);
-});
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'company_db'
+    }
+);
 
-app.listen(PORT, () => console.log(`lsitening on port ${PORT}`));
+// connection.connect(function (err) {
+//     if (err) throw err;
+// })
+
+// app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 loadMainPrompts = () => {
-    
- inquirer.prompt([
-            {
-                type: "list",
-                name: "choice",
-                message: "What would you like to do?",
-                choices: [
-                    "View All Employees",
-                    "View All Employees By Department",
-                    "View All Employees By Manager",
-                    "Add Employee",
-                    "Remove Employee",
-                    "Update Employee Role",
-                    "Update Employee Manager",
-                    "View All Roles",
-                    "Add Role",
-                    "Remove Role",
-                    "View All Departments",
-                    "Add Department",
-                    "Remove Department",
-                    "Quit",
-                ]
-            }
-        ])
-        .then ((data) => {
-            //console.log (data)
-            
-            switch (data.choice){
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "View All Roles",
+                "Add Role",
+                "Remove Role",
+                "View All Departments",
+                "Add Department",
+                "Remove Department",
+                "Quit",
+            ]
+        }
+    ])
+        .then((data) => {
+            console.log(data.choice)
+
+            switch (data.choice) {
                 case "View All Departments":
-                viewEmployeesByDept()
-                break;
-        
-                case 'view all roles':
-                viewAllRoles()
-                break;
-        
-                case 'view all employees':
-                viewAllEmployees()
-                break;
-        
+                    viewAllDepartments();
+                    break;
+
+                case 'View All Roles':
+                    viewAllRoles()
+                    break;
+
+                case 'View All Employees':
+                    viewAllEmployees()
+                    break;
+
                 case 'add a department':
-                addDepartment()
-                break;
-        
+                    addDepartment()
+                    break;
+
                 case 'add a role':
-                addRole()
-                break;
-        
+                    addRole()
+                    break;
+
                 case 'add an employee':
-                addEmployee()
-                break;
-        
+                    addEmployee()
+                    break;
+
                 case 'update an employee role':
-                updateEmployeeRole()
-                break;
-        
-                case 'exit menu':
+                    updateEmployeeRole()
+                    break;
+
+                case 'Quit':
                     console.log('Signed out. Type "npm start" to run it again')
                     connection.end();
-                break;
-                
+                    break;
+
                 default:
                     break;
-            }; //end of switch
-            
-            })
-        
-        // res.json(res);
-    // if (err) {
-    //     res.json(err);
-    // }
+            };
+
+        })
+
 }
 loadMainPrompts();
 
-function viewEmployeesByDept() {
-    connection.query('SELECT * FROM department', function (err, res) {
-        console.log('=======================')
-        console.table(res)
-    })
-    console.log('=======================')
-    loadMainPrompts();
-};
 
-const viewAllDepartments = () => {
+
+const viewAllDepartments = async () => {
     try {
         const query = 'SELECT * FROM department';
         connection.query(query, (req, res) => {
-            
+
             console.table(res);
             loadMainPrompts();
         })
-        
+
     } catch (err) {
         res.json(err);
     }
@@ -128,7 +128,7 @@ const viewAllEmployees = () => {
 
 const viewAllRoles = () => {
     try {
-        const query = 'SELECT * FROM role';
+        const query = 'SELECT * FROM roles';
         connection.query(query, (req, res) => {
             console.table(res);
             loadMainPrompts();
